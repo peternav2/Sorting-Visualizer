83017d1a84ec5848a8d684ee3e56cd3fa670a651
Peter Navarrete <navarrep1@newpaltz.edu>
2022-12-31 14:59:53 -0500

    Selection Sort added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,8 +26,8 @@ import { ref, onMounted ,  watch } from 'vue'
             await insertionSort();
         } else if (selectedSort.value === "Bubble Sort") {
             await bubbleSort();
-        } else if (selectedSort.value === "Merge Sort") {
-           await mergeSort(columns, 0, columns.length-1);
+        } else if (selectedSort.value === "Selection Sort") {
+            await selectionSort();
         }
         // console.log(columns);
     }
@@ -68,74 +68,33 @@ import { ref, onMounted ,  watch } from 'vue'
             columns[j+1] = key;
         }
     }
-    function merge(arr: number[], l: number, m: number, r: number)
+
+    function swap(arr: number[],xp: number, yp: number)
 {
-    var n1 = m - l + 1;
-    var n2 = r - m;
- 
-    // Create temp arrays
-    var L = new Array(n1);
-    var R = new Array(n2);
- 
-    // Copy data to temp arrays L[] and R[]
-    for (var i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (var j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
- 
-    // Merge the temp arrays back into arr[l..r]
- 
-    // Initial index of first subarray
-    var i = 0;
- 
-    // Initial index of second subarray
-    var j = 0;
- 
-    // Initial index of merged subarray
-    var k = l;
- 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
- 
-    // Copy the remaining elements of
-    // L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    // Copy the remaining elements of
-    // R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    var temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
 }
  
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted */
-function mergeSort(arr: number[],l: number, r: number){
-    if(l>=r){
-        return;//returns recursively
+async function selectionSort()
+{
+    var i, j, min_idx;
+     for (i = 0; i < columns.length-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < columns.length; j++) {
+            if (columns[j] < columns[min_idx])
+                min_idx = j;
+        }
+        // Swap the found minimum element with the first element
+        swap(columns,min_idx, i);
+        await sleep();
+        updateDOM.value+=1;
     }
-    var m =l+ (r-l)/2;
-    mergeSort(arr,l,m);
-    mergeSort(arr,m+1,r);
-    merge(arr,l,m,r);
 }
 
+    
     async function sleep() {
       return new Promise((resolve) => setTimeout(resolve, speedValue.value));
     }
@@ -190,8 +149,8 @@ function mergeSort(arr: number[],l: number, r: number){
                 </template>
                 <div>
                     <o-dropdown-item aria-role="listitem" value="Insertion Sort">Insertion Sort</o-dropdown-item>
-                    <o-dropdown-item aria-role="listitem" value="Insertion Sort">Bubble Sort</o-dropdown-item>
-                    <o-dropdown-item aria-role="listitem" value="Merge Sort">Merge Sort</o-dropdown-item>
+                    <o-dropdown-item aria-role="listitem" value="Bubble Sort">Bubble Sort</o-dropdown-item>
+                    <o-dropdown-item aria-role="listitem" value="Selection Sort">Selection Sort</o-dropdown-item>
                 </div>
             </o-dropdown>
         </div> 
